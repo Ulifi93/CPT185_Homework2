f888ef71876049bca978754f2ef6f96c6de1780a
Sheryl Brumbalow <slbrumbalow@gmail.com>
2020-01-24 17:56:12 -0500

    Initial Save

---
+++ CH2_Tech Job Fair Homework/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTech = New System.Windows.Forms.Label()
+        Me.lblStudent = New System.Windows.Forms.Label()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.lblLocation = New System.Windows.Forms.Label()
+        Me.lblRoom = New System.Windows.Forms.Label()
+        Me.picFair = New System.Windows.Forms.PictureBox()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picFair, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTech
+        '
+        Me.lblTech.AutoSize = True
+        Me.lblTech.Font = New System.Drawing.Font("Tahoma", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTech.Location = New System.Drawing.Point(27, 22)
+        Me.lblTech.Name = "lblTech"
+        Me.lblTech.Size = New System.Drawing.Size(270, 46)
+        Me.lblTech.TabIndex = 0
+        Me.lblTech.Text = "Tech Job Fair"
+        '
+        'lblStudent
+        '
+        Me.lblStudent.AutoSize = True
+        Me.lblStudent.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStudent.Location = New System.Drawing.Point(52, 77)
+        Me.lblStudent.Name = "lblStudent"
+        Me.lblStudent.Size = New System.Drawing.Size(225, 23)
+        Me.lblStudent.TabIndex = 1
+        Me.lblStudent.Text = "All Students Are Welcome"
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.Location = New System.Drawing.Point(92, 236)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(112, 23)
+        Me.lblDate.TabIndex = 2
+        Me.lblDate.Text = "May 2, 2016"
+        '
+        'lblLocation
+        '
+        Me.lblLocation.AutoSize = True
+        Me.lblLocation.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLocation.Location = New System.Drawing.Point(52, 285)
+        Me.lblLocation.Name = "lblLocation"
+        Me.lblLocation.Size = New System.Drawing.Size(203, 23)
+        Me.lblLocation.TabIndex = 3
+        Me.lblLocation.Text = "Located in Lochlan Hall"
+        '
+        'lblRoom
+        '
+        Me.lblRoom.AutoSize = True
+        Me.lblRoom.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRoom.Location = New System.Drawing.Point(101, 324)
+        Me.lblRoom.Name = "lblRoom"
+        Me.lblRoom.Size = New System.Drawing.Size(94, 23)
+        Me.lblRoom.TabIndex = 4
+        Me.lblRoom.Text = "Room 101"
+        '
+        'picFair
+        '
+        Me.picFair.Location = New System.Drawing.Point(321, 12)
+        Me.picFair.Name = "picFair"
+        Me.picFair.Size = New System.Drawing.Size(221, 456)
+        Me.picFair.TabIndex = 5
+        Me.picFair.TabStop = False
+        '
+        'btnView
+        '
+        Me.btnView.Location = New System.Drawing.Point(77, 126)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(168, 47)
+        Me.btnView.TabIndex = 6
+        Me.btnView.Text = "View Job Fair Details"
+        Me.btnView.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(86, 410)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(141, 44)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(554, 480)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnView)
+        Me.Controls.Add(Me.picFair)
+        Me.Controls.Add(Me.lblRoom)
+        Me.Controls.Add(Me.lblLocation)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.lblStudent)
+        Me.Controls.Add(Me.lblTech)
+        Me.Name = "Form1"
+        CType(Me.picFair, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTech As Label
+    Friend WithEvents lblStudent As Label
+    Friend WithEvents lblDate As Label
+    Friend WithEvents lblLocation As Label
+    Friend WithEvents lblRoom As Label
+    Friend WithEvents picFair As PictureBox
+    Friend WithEvents btnView As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ CH2_Tech Job Fair Homework/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CH2_Tech Job Fair Homework")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("CH2_Tech Job Fair Homework")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3d7df32c-e91d-428d-84dd-4005838ab943")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
